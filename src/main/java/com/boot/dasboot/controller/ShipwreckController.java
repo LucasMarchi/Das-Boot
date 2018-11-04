1df9bfe35573e08849523832dcf147cf2a0b5037
Lucas Marchi <lucas-marchi@hotmail.com>
2018-11-04 19:06:22 -0200

    Fourth module

--- src/main/java/com/boot/dasboot/controller/ShipwreckController.java
+++ src/main/java/com/boot/dasboot/controller/ShipwreckController.java
@@ -1,6 +1,9 @@
 package com.boot.dasboot.controller;
 
 import com.boot.dasboot.model.Shipwreck;
+import com.boot.dasboot.repository.ShipwreckRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,29 +12,36 @@ import java.util.List;
 @RequestMapping("/api/v1/shipwrecks")
 public class ShipwreckController {
 
+    @Autowired
+    private ShipwreckRepository shipwreckRepository;
+
     @GetMapping
     public List<Shipwreck> list() {
-        return ShipwreckStub.list();
+        return shipwreckRepository.findAll();
     }
 
     @PostMapping
     public Shipwreck create(@RequestBody Shipwreck shipwreck) {
-        return ShipwreckStub.create(shipwreck);
+        return shipwreckRepository.saveAndFlush(shipwreck);
     }
 
     @GetMapping("/{id}")
     public Shipwreck get(@PathVariable Long id) {
-        return ShipwreckStub.get(id);
+        return shipwreckRepository.findById(id).get();
     }
 
     @PutMapping("/{id}")
     public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck shipwreck) {
-        return ShipwreckStub.update(id, shipwreck);
+        Shipwreck existingShipwreck = shipwreckRepository.findById(id).get();
+        BeanUtils.copyProperties(shipwreck, existingShipwreck);
+        return shipwreckRepository.saveAndFlush(shipwreck);
     }
 
     @DeleteMapping("/{id}")
     public Shipwreck delete(@PathVariable Long id) {
-        return ShipwreckStub.delete(id);
+        Shipwreck existingShipwreck = shipwreckRepository.findById(id).get();
+        shipwreckRepository.delete(existingShipwreck);
+        return existingShipwreck;
     }
 
 }
